04184b9ad86917a4f5ccd666d37e00738a92c9af
PedroA-10 <pedroaugusto732@gmail.com>
2025-04-18 15:48:21 -0300

    Fix - better treatment to promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,7 +42,12 @@ public class Program {
 
                 if (chessMatch.getPromoted() != null) {
                     System.out.print("Qual peça será a promovida: B/N/Q/R: ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+                        System.out.print("Peça não existe! Qual peça será a promovida: B/N/Q/R: ");
+                        type = sc.nextLine().toUpperCase();
+
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
 
