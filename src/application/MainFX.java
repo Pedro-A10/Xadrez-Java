9d4538b21b1b5d807e1e4819fa307318caf2a90a
Pedro A. <pedroaugusto732@gmail.com>
2025-08-26 19:31:49 -0300

    Correções de erros

--- src/application/MainFX.java
+++ src/application/MainFX.java
@@ -1,22 +1,22 @@
 package application;
 
-    import javafx.application.Application;
-    import javafx.fxml.FXMLLoader;
-    import javafx.scene.Scene;
-    import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public class MainFX extends Application {
+public class MainFX extends Application {
 
-        @Override
-        public void start(Stage primaryStage) throws Exception {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ViewController/View.fxml"));
-            Scene scene = new Scene(loader.load());
-            primaryStage.setTitle("Xadrez JavaFX");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-            }
-        public static void main(String[] args) {
-                launch(args);
-            }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/resource/View.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Xadrez JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/resource/ViewController.java
+++ src/resource/ViewController.java
@@ -11,8 +11,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
-
-
 public class ViewController {
 
     @FXML
@@ -26,6 +24,14 @@ public class ViewController {
     @FXML
     public void initialize() {
         chessMatch = new ChessMatch();
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                Label label = new Label();
+                boardLabels[row][col] = label;
+                boardGrid.add(label, col, row);
+                label.setStyle("-fx-font-size: 24px; -fx-alignment: center;");
+            }
+        }
         for (Node node : boardGrid.getChildren()) {
             Integer row = GridPane.getRowIndex(node);
             Integer col = GridPane.getColumnIndex(node);
@@ -33,12 +39,6 @@ public class ViewController {
             col = col == null ? 0 : col;
             if (node instanceof Button) {
                 boardButtons[row][col] = (Button) node;
-            } else if (node instanceof Label) {
-                boardLabels[row][col] = (Label) node;
-            }
-        }
-        for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 8; col++) {
                 final int r = row, c = col;
                 boardButtons[row][col].setOnAction(e -> handleCellClick(r, c));
             }
@@ -70,20 +70,18 @@ public class ViewController {
         } else {
             char sourceColumn = (char) ('a' + selectedPosition.getColumn());
             int sourceRow = 8 - selectedPosition.getRow();
-
             char targetColumn = (char) ('a' + col);
             int targetRow = 8 - row;
 
             ChessPosition source = new ChessPosition(sourceColumn, sourceRow);
             ChessPosition target = new ChessPosition(targetColumn, targetRow);
-
             chessMatch.performChessMove(source, target);
             selectedPosition = null;
             updateBoard();
         }
     }
 
-    private String translateLetterToEmoji (String letter, Color color){
+    private String translateLetterToEmoji(String letter, Color color) {
         switch (letter.toUpperCase()) {
             case "K":
                 return color == Color.WHITE ? "♔" : "♚";
@@ -99,6 +97,6 @@ public class ViewController {
                 return color == Color.WHITE ? "♙" : "♟";
             default:
                 return "";
-            }
         }
     }
+}
